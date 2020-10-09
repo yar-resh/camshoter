f39503a691a3a7037be8b87d65ddd2e80fdb2012
Yaroslav Reshetnyk <reshetnik.yaroslav@gmail.com>
2020-10-09 20:29:42 +0300

    Add two modes
    - Add mode in which frames will be captured on button press
    - Add mode in which frames will be captured instantly on script run

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This script is intended to poll web-cameras plugged into Raspberry Pi and save i
 
 ### Requirements
 ```shell script
+sudo apt-get install python3-rpi.gpio
 sudo apt-get install libv4l-dev
 sudo pip3 install -r requirements.txt
 ```
--- camshoter.py
+++ camshoter.py
@@ -5,6 +5,7 @@ import glob
 import logging
 import os
 import sys
+import threading
 import time
 
 import PIL.Image
@@ -12,6 +13,10 @@ import PyV4L2Camera.camera
 import PyV4L2Camera.exceptions
 
 DEFAULT_DIR = 'images'
+DEFAULT_GPIO_PIN_NUMBER = 3
+IMAGE_FORMAT = 'jpeg'
+
+LOG = logging.getLogger("camshoter")
 
 
 def get_full_path(path):
@@ -26,28 +31,7 @@ def get_full_path(path):
     return os.path.join(script_directory, path)
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='This script polls web-cameras plugged into Raspberry Pi '
-                    'and saves images to directory (specified or default)')
-    parser.add_argument('-d', '--directory', action='store', default=DEFAULT_DIR, dest='image_directory',
-                        help='path to the directory where images should be located. Can be either absolute or relative')
-    args = parser.parse_args()
-
-    logger = logging.getLogger("camshoter")
-
-    image_directory = get_full_path(args.image_directory)
-    if not os.path.exists(image_directory):
-        try:
-            os.makedirs(image_directory, exist_ok=True)
-        except OSError as err:
-            logger.error(f"error creating directory {image_directory}: {str(err)}")
-            sys.exit(1)
-    else:
-        if not os.access(image_directory, os.W_OK):
-            logger.error(f"no permissions to write into {image_directory}")
-            sys.exit(1)
-
+def save_frames(image_directory):
     current_day = datetime.datetime.now()
     current_timestamp = int(time.time())
     current_day_str = current_day.strftime("%Y-%m-%d")
@@ -62,11 +46,58 @@ def main():
             with contextlib.closing(PyV4L2Camera.camera.Camera(video_device)) as camera:
                 frame = camera.get_frame()
         except PyV4L2Camera.exceptions.CameraError:
+            LOG.warning(f'device {video_device} is unavailable')
             continue
 
         image = PIL.Image.frombytes('RGB', (camera.width, camera.height), frame)
-        image.save(f'{os.path.join(current_image_directory, str(device_number))}.jpg', format='JPEG')
+        image.save(f'{os.path.join(current_image_directory, str(device_number))}.{IMAGE_FORMAT}', format=IMAGE_FORMAT)
         device_number += 1
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='This script polls web-cameras plugged into Raspberry Pi '
+                    'and saves images to directory (specified or default)')
+    parser.add_argument('-d', '--directory', action='store', default=DEFAULT_DIR, dest='image_directory',
+                        help='path to the directory where images should be located. Can be either absolute or relative')
+    parser.add_argument('-i', '--instant', action='store_true', dest='instant',
+                        help='frames from cameras will be captured instantly, without waiting for button press; '
+                             'script will exit immediately after saving frames')
+    parser.add_argument('-p', '--pin_number', action='store', default=DEFAULT_GPIO_PIN_NUMBER, dest='gpio_pin_number',
+                        type=int, help='number of GPIO pin button connected to')
+    args = parser.parse_args()
+
+    image_directory = get_full_path(args.image_directory)
+    if not os.path.exists(image_directory):
+        try:
+            os.makedirs(image_directory, exist_ok=True)
+        except OSError as err:
+            LOG.error(f'error creating directory {image_directory}: {str(err)}')
+            sys.exit(1)
+    else:
+        if not os.access(image_directory, os.W_OK):
+            LOG.error(f'no permissions to write into {image_directory}')
+            sys.exit(1)
+
+    video_devices = glob.glob('/dev/video*')
+    if not video_devices:
+        LOG.warning('there is no video devices in system')
+        sys.exit(0)
+
+    if args.instant:
+        save_frames(image_directory)
+    else:
+        # RPi.GPIO can be imported only on Raspberry Pi
+        import RPi.GPIO as GPIO
+
+        def callback(channel):
+            save_frames(image_directory)
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(args.pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(10, GPIO.RISING, callback=callback)
+
+        threading.Event().wait()
 
 
 if '__main__' == __name__:
